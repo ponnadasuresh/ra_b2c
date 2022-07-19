3f4e96169ec9aa4cfe01c7e549a37bb8adb52c99
suresh <sureshbabu733@gmail.com>
2022-07-19 17:13:59 +0530

    subscription booknow

--- src/app/newlayoutcomponents/nl-subscriptionform/nl-subscriptionform.component.ts
+++ src/app/newlayoutcomponents/nl-subscriptionform/nl-subscriptionform.component.ts
@@ -12,18 +12,82 @@ export class NlSubscriptionformComponent implements OnInit {
   isEnabled: boolean | undefined;
   signIn: string = "Proceed →";
   isAddnewvehicleEnable: boolean | undefined;
-
   totalSteps = 5;
   steps: number = 1
+  cardactive =  false;
+  VehiclesListenable = true;
+  addVehiclesFormenable = false;
+  vehiclemodel = 'Choose vehicle model'
 
+
+  // Vehicles model list "Hero Xtreme 200S", "Hero Destini 125", "Hero Maestro Edge 125"
+  vehiclesModelList: any = [
+    {
+      id:1,
+      vehiclemodel: 'Hero Xtreme 200S',
+    },
+    {
+      id:2,
+      vehiclemodel: 'yamaha',
+    },
+    {
+      id:3,
+      vehiclemodel: ' Maestro Edge 125',
+    },
+    {
+      id:4,
+      vehiclemodel: 'Hero Xpulse 200T',
+    },
+  ];
+  // Vehicles model
+
+
+  // list of vehicles
+  vehiclesList= [
+    {
+      id:1,
+      vehiclename: 'Hero Splendor Plus',
+      vehiclenumber: 'KA23',
+      isSelect:false
+    },
+    {
+      id:2,
+      vehiclename: 'Hero Splendor Plus',
+      vehiclenumber: 'KA23',
+      isSelect:false
+    },
+    {
+      id:3,
+      vehiclename: 'Hero Splendor Plus',
+      vehiclenumber: 'KA23',
+      isSelect:false
+    },
+    {
+      id:4,
+      vehiclename: 'Hero Splendor Plus',
+      vehiclenumber: 'KA23',
+      isSelect:false
+    },
+  ]
 
   ngOnInit(): void {
   }
 
+  selectVehicle(v: any) {
+    for (const x of this.vehiclesList) {
+      Object.assign(x, {isSelect: false});
+    }
+    this.vehiclesList.filter((y: any) => v.id === y.id)[0].isSelect = true;
+  }
+
   addnewVehicle(){
-this.isAddnewvehicleEnable = true;
+this.isAddnewvehicleEnable = false;
+this.VehiclesListenable = false;
+this.addVehiclesFormenable = true;
 this.signIn = "Proceed →"
   }
+
+
 
   next() {
     // console.log(this.steps);
