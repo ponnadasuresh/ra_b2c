a3df027284fa02b8878c404c28bcb4f351a98d1d
suresh <ponnada.suresh@readyassist.in>
2022-08-04 16:38:16 +0530

    subscriptionform list of vehicles flow development done

--- src/app/newlayoutcomponents/nl-subscription-conformation/nl-subscription-conformation.component.ts
+++ src/app/newlayoutcomponents/nl-subscription-conformation/nl-subscription-conformation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var window: any;
 @Component({
   selector: 'app-nl-subscription-conformation',
   templateUrl: './nl-subscription-conformation.component.html',
@@ -9,7 +9,19 @@ export class NlSubscriptionConformationComponent implements OnInit {
 
   constructor() { }
 
+  formModal: any;
+
+  openFormModal() {
+    this.formModal.show();
+  }
+  saveSomeThing() {
+    // confirm or save something
+    this.formModal.hide();
+  }
+
   ngOnInit(): void {
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('exampleModal'))
   }
 
 }
